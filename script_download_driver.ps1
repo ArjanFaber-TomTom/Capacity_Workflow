51457d8167ae7bd1997f86c950e8daeb97cc49ef
ArjanFaber-TomTom <arjan.faber@tomtom.com>
2025-08-19 14:30:38 +0200

    Update script_download_driver.ps1

--- script_download_driver.ps1
+++ script_download_driver.ps1
@@ -1,5 +1,5 @@
 # Path to your MSI
-$installerPath = ".\x64_17.0.861.0_SQL_AS_OLEDB.msi"
+$installerPath = ".\msoledbsql.msi"
 
 # Check if the file exists
 if (-Not (Test-Path $installerPath)) {
@@ -46,5 +46,6 @@ try {
 } catch {
     Write-Warning "Could not enumerate ADODB providers. The driver might not be installed correctly."
 }
+
 
 
